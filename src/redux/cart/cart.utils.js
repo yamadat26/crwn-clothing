45be8253b78d113fb9e6202fd2d2c0525f1bd2d1
山田隆史 <takashiyamada@yamadatakashinoMacBook-Air.local>
2020-07-16 15:07:07 +0900

    updated shop data and updated cart utils

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,16 @@
+export const addItemCart = (cartItems, cartItemAdd) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemAdd.id
+    );
+
+  if(existingCartItem) {
+    return cartItems.map(
+      cartItem => 
+      cartItem.id === cartItemAdd.id
+        ? {...cartItem, quantity: cartItem.quantity + 1}
+        : cartItem
+       )
+
+  }
+    return [...cartItems, { ...cartItemAdd, quantity: 1}]
+};
