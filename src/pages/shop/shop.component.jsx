dbd05ada28872796d5493455a586f52d6f8fc259
山田隆史 <takashiyamada@yamadatakashinoMacBook-Air.local>
2020-07-17 17:40:37 +0900

    Updating with selector

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,30 +1,12 @@
 import React from 'react';
 
-import SHOP_DATA from './shop.data';
+import CollectionOverview from '../../components/collections-overview/collections-overview.componet'
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+const ShopPage = ({collections}) => (
+  <div className='shop-page'>
+    <CollectionOverview />
+  </div>
+)
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA
-    }
-  }
-
-  render() {
-    const {collections} = this.state
-    return (
-      <div className='shop-page'>
-        {
-          collections.map(({id, ...otherCollectionProps}) => (
-            <CollectionPreview key={id} {...otherCollectionProps} />
-          ))
-        }
-      </div>
-    )
-  }
-}
 
 export default ShopPage;
