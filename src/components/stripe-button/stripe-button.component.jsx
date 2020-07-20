1c40144f67fad3a9181505afa20c9aa2a6937e99
山田隆史 <takashiyamada@yamadatakashinoMacBook-Air.local>
2020-07-20 12:24:31 +0900

    Updating stripe intergration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51H6oWVBca9BJ1ULGycxoDg5pwY0kw9Pr55ndYWOSzAwigFnD0Wjg4B2WygQwiz6HUnyxtO8n431vVVfuY6uklxpB00HBSZ5WEs';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful');
+  }
+
+  return (
+    <StripeCheckout 
+      label='Pay Now'
+      name='CRWN Clothing LTD.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
